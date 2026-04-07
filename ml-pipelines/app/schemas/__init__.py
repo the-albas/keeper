e0362dc5adacd7a106383af5b1c6ea4450b04b49
ethanhousley1 <ethanhousley@gmail.com>
2026-04-07 12:26:57 -0600

    one line fix gang

--- ml-pipelines/app/schemas/__init__.py
+++ ml-pipelines/app/schemas/__init__.py
@@ -1,5 +1,3 @@
 from app.schemas.retention import RetentionFeatures, RetentionPrediction
 
-__all__ = ["RetentionFeatures", "RetentionPrediction", "GirlsStrugglingFeatures", "GirlsStrugglingPrediction"]
-
-from app.schemas.girls_struggling import GirlsStrugglingFeatures, GirlsStrugglingPrediction
+__all__ = ["RetentionFeatures", "RetentionPrediction"]
