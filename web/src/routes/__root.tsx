efff4d03f87a9548a3fdf55a3506ef6439eb43b7
broc-mcu <bcuartas@byu.edu>
2026-04-07 14:53:06 -0600

    Got rid of weird thing at bottom of screen

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,11 +1,9 @@
 import type { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const RootLayout = () => (
   <>
     <Outlet />
-    <TanStackRouterDevtools />
   </>
 );
 
