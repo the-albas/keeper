294d6de494ce5efb8cf1fd313840fbf9a7011605
ethanhousley1 <ethanhousley@gmail.com>
2026-04-08 09:38:55 -0600

    more homepage fixes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,9 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Navbar from "@/components/landing/Navbar";
 import HeroSection from "@/components/landing/HeroSection";
-import ImpactStats from "@/components/landing/ImpactStats";
-import type { ImpactStatsData } from "@/components/landing/ImpactStats";
-import AboutSection from "@/components/landing/AboutSection";
 import DonateSection from "@/components/landing/DonateSection";
 import MoneyFlow from "@/components/landing/MoneyFlow";
 import LandingFooter from "@/components/landing/LandingFooter";
@@ -12,21 +9,13 @@ export const Route = createFileRoute("/")({
   component: Landing,
 });
 
-const landingImpactStats: ImpactStatsData = {
-  girlsServed: "250+",
-  safehouses: "5",
-  reintegration: "87%",
-};
-
 function Landing() {
   return (
     <div className="min-h-screen bg-background font-body flex flex-col">
       <Navbar />
       <HeroSection />
-      <ImpactStats stats={landingImpactStats} />
-      <AboutSection />
-      <MoneyFlow />
       <DonateSection />
+      <MoneyFlow />
       <LandingFooter />
     </div>
   );
